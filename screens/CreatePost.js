fe3181f59e0b86488fbd4477d55bd07fbd780b12
Vivek Yadav <88832797+vivekyadav14302@users.noreply.github.com>
2022-05-07 15:27:44 +0530

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function CreatePost() {
+  return (
+    <Text>CreatePost Screen</Text>
+  )
+}
+
+export default CreatePost
---
+++ screens/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function Profile() {
+  return (
+    <Text>Profile Screen</Text>
+  )
+}
+
+export default Profile
